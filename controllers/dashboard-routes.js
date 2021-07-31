f46845504ac9cd33ef8d95855bffd91fe004074f
Marlon Liemann <awesomemarlon@gmail.com>
2021-07-31 10:38:13 -0400

    Add PUT & DELETE

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -46,4 +46,57 @@ router.get('/edit/:id', withAuth, async (req, res) => {
   }
 });
 
+router.post("/", async  (req, res) => {
+  // create a new category
+  try {
+    const catData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", async  (req, res) => {
+  // update a category by its `id` value
+  try {
+    const catData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!catData) {
+      res.status(404).json({ message: "No category found with provided ID" });
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async  (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const catData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!catData) {
+      res.status(404).json({ message: "No category found with provided ID" });
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
