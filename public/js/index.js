15b9aa11e5285a1a751a4582f521ea62dbc93d57
Tyler Davis <tdnc85@yahoo.com>
2021-08-04 20:09:26 -0400

    got images working

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,25 @@
-const imageForm = document.querySelector("#imageForm");
-const imageInput = document.querySelector("#imageInput");
-var imageUrl
-imageForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const file = imageInput.files[0];
+// const imageForm = document.querySelector("#imageForm");
+// const imageInput = document.querySelector("#imageInput");
+// var imageUrl
+// imageForm.addEventListener("submit", async (event) => {
+//   event.preventDefault();
+//   const file = imageInput.files[0];
 
-  // get secure url from our server
-  const { url } = await fetch("/s3Url").then((res) => res.json());
-  console.log(url);
+//   // get secure url from our server
+//   const { url } = await fetch("/s3Url").then((res) => res.json());
+//   console.log(url);
 
-  // post the image direclty to the s3 bucket
-  await fetch(url, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-    body: file,
-  });
+//   // post the image direclty to the s3 bucket
+//   await fetch(url, {
+//     method: "PUT",
+//     headers: {
+//       "Content-Type": "image/jpeg",
+//     },
+//     body: file,
+//   });
 
-  imageUrl = url.split("?")[0];
-  console.log(imageUrl);
+//   imageUrl = url.split("?")[0];
+//   console.log(imageUrl);
 
   // post requst to my server to store any extra data
   
@@ -27,6 +27,6 @@ imageForm.addEventListener("submit", async (event) => {
   // const img = document.createElement("img");
   // img.src = imageUrl;
   // document.body.appendChild(img);
-});
+// });
 
 
